15e30ebb0766277cf416724c0fa80cbc25ea2009
K, Raji (Cognizant) <576215@cognizant.com>
2020-08-23 19:33:25 +0100

    Added the swagger

--- TokenManagementSystem/Startup.cs
+++ TokenManagementSystem/Startup.cs
@@ -29,11 +29,24 @@ namespace TokenManagementSystem
         {
             services.AddControllers();
             services.AddSingleton<ITokenCosmosDBService>(InitializeCosmosClientInstanceAsync(Configuration.GetSection("CosmosDb")).GetAwaiter().GetResult());
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TMS");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
